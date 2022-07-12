1d617a1f7c53dd196b0fe9398991437480a63d93
Bryan Olsson <bolsson@mozilla.com>
2022-07-12 15:28:02 -0700

    First attempt at getting action to work

--- .github/scripts/unchaged-ids/fluent.syntax/parse_messages_terms_to_dict.py
+++ .github/scripts/unchaged-ids/fluent.syntax/parse_messages_terms_to_dict.py
@@ -1,5 +1,11 @@
+import argparse
 from fluent.syntax import parse, ast
 from fluent.syntax.serializer_value import FluentSerializerValue
+
+parser = argparse.ArgumentParser()
+parser.add_argument("old")
+parser.add_argument("new")
+args = parser.parse_args()
 
 serializer = FluentSerializerValue()
 
@@ -15,8 +21,8 @@ def find_changed_ids(file_old, file_new):
     return {key: {value, new_dict[key]} for key, value in old_dict.items() if key in new_dict and new_dict[key] != value}
 
 def main():
-    file_old = "en-old.ftl"
-    file_new = "en-new.ftl"
+    file_old = args.old
+    file_new = args.new
 
     changed_ids = find_changed_ids(file_old, file_new)
 
