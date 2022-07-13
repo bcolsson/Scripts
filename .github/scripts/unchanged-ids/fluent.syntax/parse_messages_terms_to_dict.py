9421bf4a49fd9dc09ddc90d8efab8141a7b351f3
Bryan Olsson <bolsson@mozilla.com>
2022-07-13 06:12:17 -0700

    Added error handling. Additional changes to action for teting.

--- .github/scripts/unchanged-ids/fluent.syntax/parse_messages_terms_to_dict.py
+++ .github/scripts/unchanged-ids/fluent.syntax/parse_messages_terms_to_dict.py
@@ -1,19 +1,17 @@
 import argparse
+import sys
 from fluent.syntax import parse, ast
 from fluent.syntax.serializer_value import FluentSerializerValue
-
-parser = argparse.ArgumentParser()
-parser.add_argument("old")
-parser.add_argument("new")
-args = parser.parse_args()
 
 serializer = FluentSerializerValue()
 
 def generate_dict_from_file(file):
-    fn = open(file)
-    resource = parse(fn.read())
-    fn.close()
-    return {entry.id.name: serializer.serialize_entry(entry) for entry in resource.body if isinstance(entry, ast.Message) or isinstance(entry, ast.Term)}
+    try:
+        with open(file) as f:
+            resource = parse(f.read())
+            return {entry.id.name: serializer.serialize_entry(entry) for entry in resource.body if isinstance(entry, ast.Message) or isinstance(entry, ast.Term)}
+        except Exception as e:
+            sys.exit(e)
 
 def find_changed_ids(file_old, file_new):
     old_dict = generate_dict_from_file(file_old)
@@ -21,6 +19,11 @@ def find_changed_ids(file_old, file_new):
     return {key: {value, new_dict[key]} for key, value in old_dict.items() if key in new_dict and new_dict[key] != value}
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("old")
+    parser.add_argument("new")
+    args = parser.parse_args()
+
     file_old = args.old
     file_new = args.new
 
