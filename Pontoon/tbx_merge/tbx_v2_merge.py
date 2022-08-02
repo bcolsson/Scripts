bcdfb8bb0bd2e23d7ab329d5c4486eae07724f49
Bryan Olsson <bolsson@mozilla.com>
2022-08-01 17:49:36 -0700

    Compare pontoon export with smartling, update IDs to match for import

--- Pontoon/tbx_merge/tbx_v2_merge.py
+++ Pontoon/tbx_merge/tbx_v2_merge.py
@@ -50,7 +50,7 @@ class XMLCombiner(object):
                     # Just add it
                     one.append(el)
 
-def remove_ids(f):
+def remove_all_ids(f):
     """Smartling cannot use IDs assigned by Pontoon, so they must be removed to import as a new glossary"""
     result = ""
     with open(f) as file:
@@ -59,9 +59,45 @@ def remove_ids(f):
     with open(f, "w") as file:
         file.write(result)
 
+def remove_new_ids(f):
+    pass
+
+def extract_smartling_id_term(f):
+    root = et.parse(f).getroot()
+    smartling_dict = {}
+    for termEntry in root.iter('termEntry'):
+        term = termEntry.find("./langSet[@{http://www.w3.org/XML/1998/namespace}lang='en-US']/tig/term").text
+        try:
+            definition = termEntry.find("./descrip[@type='definition']").text
+        except AttributeError:
+            definition = None
+        id = termEntry.attrib['id']
+        smartling_dict[(term, definition)] = id
+
+    return smartling_dict
+
+def convert_ids(f, smartling_dict):
+    root = et.parse(f).getroot()
+    pontoon_term_map = {}
+    for termEntry in root.iter('termEntry'):
+        term = termEntry.find("./langSet[@{http://www.w3.org/XML/1998/namespace}lang='en-US']/ntig/termGrp/term").text
+        try:
+            definition = termEntry.find("./langSet[@{http://www.w3.org/XML/1998/namespace}lang='en-US']/descripGrp/descrip[@type='definition']").text
+        except AttributeError:
+            definition = None
+        pontoon_term_map[(term, definition)] = termEntry
+    
+    for key, value in smartling_dict.items():
+        if key in pontoon_term_map:
+            pontoon_term_map[key].attrib['id'] = value
+    
+    return et.ElementTree(root)
 
 if __name__ == '__main__':
 
     r = XMLCombiner(sys.argv[1:]).combine()
-    r.write('test.tbx', encoding="UTF-8", xml_declaration=True)
-    remove_ids('test.tbx')
+    r.write('import_test.tbx', encoding="UTF-8", xml_declaration=True)
+    smartling_dict = extract_smartling_id_term('import_test_export.tbx')
+    w = convert_ids('import_test.tbx', smartling_dict)
+    w.write('test_test_modifications.tbx', encoding="UTF-8", xml_declaration=True)
+    remove_all_ids('import_test.tbx')
